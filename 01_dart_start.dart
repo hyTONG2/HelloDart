44e4071f661f2aae62a3257eff0b5e3a28015d4c
hyTONG2 <1548327816@qq.com>
2022-03-31 08:43:33 +0000

    新增流程控制函数

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -10,7 +10,32 @@ void variables(){
   };
   print('name is $name,year is $year,antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects,image is $image');
 }
+
+//流程控制语句 https://dart.cn/samples#control-flow-statements
+void flow_control(){
+   var year=1997;
+   if(year>=2001){
+     print('21st century');
+   }else if(year>=1901){
+     print('20th century');
+   }
+   
+   const flybyObjects=['Obj1','Obj2','Obj3'];
+   for(final object in flybyObjects){
+     print(object);
+   }
+
+   for(int month=1;month<=12;month++){
+     print(month);
+   }
+
+   while(year<2016){
+     year+=1;
+   }
+   print('year is $year');
+}
 void main(List<String> args){
   //变量
   variables();
+  flow_control();
 }
